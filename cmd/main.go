8879fc714c0dc1fcd2080650eeb84cb609973827
René Bizelli <renebizelli@gmail.com>
2025-03-11 17:58:58 -0300

    logic draft

--- cmd/main.go
+++ cmd/main.go
@@ -4,7 +4,6 @@ import (
 	"context"
 	"fmt"
 	"net/http"
-	"time"
 
 	"github.com/renebizelli/ratelimiter/configs"
 	"github.com/renebizelli/ratelimiter/internal/infra/middlewares"
@@ -26,22 +25,6 @@ func main() {
 	x := rdb.Ping(ctx)
 	fmt.Println(x)
 
-	fmt.Println(time.Now().UnixMilli())
-
-	e := rdb.Set(ctx, "key", "1", time.Second).Err()
-	if e != nil {
-		fmt.Println(e)
-	}
-
-	v, e := rdb.Get(ctx, "key").Result()
-	if e != nil {
-		fmt.Println(e)
-	}
-	fmt.Println(v)
-
-	time.Sleep(1 * time.Second)
-	v = rdb.Get(ctx, "key").Val()
-	fmt.Println(v)
 	cfgs := configs.LoadConfig("./")
 
 	mux := http.NewServeMux()
@@ -49,8 +32,10 @@ func main() {
 	songWebServer := webserver.NewSongWebServer()
 	loginWebserver := webserver.NewLoginWebServer(cfgs.JWTToken, cfgs.JWTExpires)
 
+	ratelimiter := middlewares.NewRateLimiter(rdb, cfgs)
+
 	mux.HandleFunc("POST /login", loginWebserver.LoginHandler)
-	mux.Handle("GET /songs", middlewares.RateLimiter(cfgs, http.HandlerFunc(songWebServer.GetSongsHandler)))
+	mux.Handle("GET /songs", ratelimiter.Limiter(http.HandlerFunc(songWebServer.GetSongsHandler)))
 
 	fmt.Printf("Web server running on port %s\n", cfgs.WebServerPort)
 
--- internal/infra/middlewares/ratelimiter_redis.go
+++ internal/infra/middlewares/ratelimiter_redis.go
@@ -1,14 +1,30 @@
 package middlewares
 
 import (
+	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"time"
 
 	"github.com/go-chi/jwtauth"
+	"github.com/redis/go-redis/v9"
 	"github.com/renebizelli/ratelimiter/configs"
 )
 
-func RateLimiter(configs *configs.Config, next http.Handler) http.Handler {
+type RateLimiter struct {
+	db     *redis.Client
+	config *configs.Config
+}
+
+func NewRateLimiter(db *redis.Client, config *configs.Config) *RateLimiter {
+	return &RateLimiter{
+		db:     db,
+		config: config,
+	}
+}
+
+func (l *RateLimiter) Limiter(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 
 		tokenString := jwtauth.TokenFromHeader(r)
@@ -20,7 +36,7 @@ func RateLimiter(configs *configs.Config, next http.Handler) http.Handler {
 			return
 		}
 
-		_, e := jwtauth.VerifyToken(configs.JWTToken, tokenString)
+		_, e := jwtauth.VerifyToken(l.config.JWTToken, tokenString)
 
 		if e != nil {
 			w.Header().Set("Content-Type", "application/json")
@@ -29,13 +45,45 @@ func RateLimiter(configs *configs.Config, next http.Handler) http.Handler {
 			return
 		}
 
-		token, _ := configs.JWTToken.Decode(tokenString)
+		token, _ := l.config.JWTToken.Decode(tokenString)
 		sub, _ := token.Get("sub")
 
-		log.Println("Authorization: ", token)
-		log.Println("claims: ", sub)
+		counterKey := fmt.Sprintf("%s:%d", sub, time.Now().Second())
+		blockedKey := fmt.Sprintf("%s:blocked", sub)
 
-		log.Println("claims: ", r.RemoteAddr)
+		blocked, _ := l.db.Get(r.Context(), blockedKey).Result()
+
+		if blocked != "" {
+			w.Header().Set("Content-Type", "application/json")
+			w.WriteHeader(429)
+			w.Write([]byte(`{"message": "blocked"}`))
+			return
+		}
+
+		count, e := l.db.Get(r.Context(), counterKey).Result()
+
+		if e != nil {
+			fmt.Println("2 >>", e)
+			l.db.Set(r.Context(), counterKey, "1", time.Duration(10*time.Second))
+			next.ServeHTTP(w, r)
+			return
+		}
+
+		intCount, _ := strconv.Atoi(count)
+
+		if intCount > 3 {
+			log.Println("blocked")
+			l.db.Set(r.Context(), blockedKey, 0, time.Duration(10*time.Second))
+			w.WriteHeader(429)
+			return
+		}
+
+		l.db.Set(r.Context(), counterKey, intCount+1, time.Duration(10*time.Second))
+
+		log.Println("claims: ", counterKey)
+
+		log.Println("RemoteAddr: ", r.RemoteAddr)
+		log.Println("intValue: ", intCount)
 
 		//w.Header().Set("Content-Type", "application/json")
 		//w.WriteHeader(429)
